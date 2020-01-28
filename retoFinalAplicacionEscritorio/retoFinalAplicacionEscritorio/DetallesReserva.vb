6e781e327d7472b208596c7c404a8e8f7ad837cb
xpCRYSISqx <mikel.gozaloqu@elorrieta-errekamari.com>
2020-01-28 12:07:07 +0100

    Controladas las posibles excepciones del formulario de detalles de reserva

--- retoFinalAplicacionEscritorio/retoFinalAplicacionEscritorio/DetallesReserva.vb
+++ retoFinalAplicacionEscritorio/retoFinalAplicacionEscritorio/DetallesReserva.vb
@@ -23,21 +23,27 @@ Public Class DetallesReserva
 		CargarReserva()
 		Dim adapter As New MySqlDataAdapter("SELECT `dni` FROM alojamientos_fac.usuarios", conexion)
 		Dim tabla As New DataTable()
-
-		adapter.Fill(tabla)
-		dniUsu.DataSource = tabla
-		dniUsu.ValueMember = "dni"
-		dniUsu.DisplayMember = "dni"
-		dniUsu.SelectedValue = dni
+		Try
+			adapter.Fill(tabla)
+			dniUsu.DataSource = tabla
+			dniUsu.ValueMember = "dni"
+			dniUsu.DisplayMember = "dni"
+			dniUsu.SelectedValue = dni
+		Catch ex As MySqlException
+			MsgBox("Error al cargar los usuarios", MsgBoxStyle.DefaultButton1 + MsgBoxStyle.Critical, "Error")
+		End Try
 
 		Dim adapter2 As New MySqlDataAdapter("SELECT `signatura`, `documentname` FROM alojamientos_fac.alojamientos ORDER BY `documentname` ASC", conexion)
 		Dim tabla2 As New DataTable()
-
-		adapter2.Fill(tabla2)
-		nombreAloj.DataSource = tabla2
-		nombreAloj.ValueMember = "signatura"
-		nombreAloj.DisplayMember = "documentname"
-		nombreAloj.SelectedValue = codigo
+		Try
+			adapter2.Fill(tabla2)
+			nombreAloj.DataSource = tabla2
+			nombreAloj.ValueMember = "signatura"
+			nombreAloj.DisplayMember = "documentname"
+			nombreAloj.SelectedValue = codigo
+		Catch ex As MySqlException
+			MsgBox("Error al cargar los alojamientos", MsgBoxStyle.DefaultButton1 + MsgBoxStyle.Critical, "Error")
+		End Try
 
 		CargarCliente()
 		CargarAlojamiento()
@@ -50,39 +56,48 @@ Public Class DetallesReserva
 	Private Sub CargarReserva()
 		Dim adapter As New MySqlDataAdapter("SELECT `id`, `dni`, `fecha_entrada`, `fecha_salida`, `alojamiento`, `personas` FROM alojamientos_fac.reservas WHERE id=" & cod, conexion)
 		Dim tabla As New DataTable()
-
-		adapter.Fill(tabla)
-		dni = tabla(0)(1)
-		fechaEntrada.Value = tabla(0)(2)
-		fechaSalida.Value = tabla(0)(3)
-		codigo = tabla(0)(4)
-		codigoRes.Text = cod
-		numeroPersonas.Value = tabla(0)(5)
+		Try
+			adapter.Fill(tabla)
+			dni = tabla(0)(1)
+			fechaEntrada.Value = tabla(0)(2)
+			fechaSalida.Value = tabla(0)(3)
+			codigo = tabla(0)(4)
+			codigoRes.Text = cod
+			numeroPersonas.Value = tabla(0)(5)
+		Catch ex As MySqlException
+			MsgBox("Error al recuperar la información de la reserva", MsgBoxStyle.DefaultButton1 + MsgBoxStyle.Critical, "Error")
+		End Try
 	End Sub
 
 	Private Sub CargarCliente()
 		Dim comando As New MySqlCommand("SELECT `nombre`, `apellido` FROM alojamientos_fac.usuarios WHERE dni = @dni", conexion)
 		comando.Parameters.Add("@dni", MySqlDbType.VarChar).Value = dniUsu.SelectedValue
+		Try
+			Dim adapter As New MySqlDataAdapter(comando)
+			Dim tabla As New DataTable()
 
-		Dim adapter As New MySqlDataAdapter(comando)
-		Dim tabla As New DataTable()
-
-		adapter.Fill(tabla)
-		nombreUsu.Text = tabla(0)(0)
-		apellidoUsu.Text = tabla(0)(1)
+			adapter.Fill(tabla)
+			nombreUsu.Text = tabla(0)(0)
+			apellidoUsu.Text = tabla(0)(1)
+		Catch ex As MySqlException
+			MsgBox("Error al recuperar la información del cliente de la base de datos", MsgBoxStyle.DefaultButton1 + MsgBoxStyle.Critical, "Error")
+		End Try
 	End Sub
 
 	Private Sub CargarAlojamiento()
 		Dim comando As New MySqlCommand("SELECT `signatura`, `capacity`, `lodgingtype` FROM alojamientos_fac.alojamientos WHERE signatura = @signatura", conexion)
 		comando.Parameters.Add("@signatura", MySqlDbType.VarChar).Value = nombreAloj.SelectedValue
+		Try
+			Dim adapter As New MySqlDataAdapter(comando)
+			Dim tabla As New DataTable()
 
-		Dim adapter As New MySqlDataAdapter(comando)
-		Dim tabla As New DataTable()
-
-		adapter.Fill(tabla)
-		codigoAloj.Text = tabla(0)(0)
-		capacidadAloj.Text = tabla(0)(1)
-		tipo = tabla(0)(2)
+			adapter.Fill(tabla)
+			codigoAloj.Text = tabla(0)(0)
+			capacidadAloj.Text = tabla(0)(1)
+			tipo = tabla(0)(2)
+		Catch ex As MySqlException
+			MsgBox("Error al recuperar la información del alojamiento de la base de datos", MsgBoxStyle.DefaultButton1 + MsgBoxStyle.Critical, "Error")
+		End Try
 	End Sub
 
 	Private Sub DniUsu_SelectionChangeCommitted(sender As Object, e As EventArgs) Handles dniUsu.SelectionChangeCommitted
@@ -174,18 +189,21 @@ Public Class DetallesReserva
 		comando.Parameters.Add("@alojamiento", MySqlDbType.VarChar).Value = codigoAloj.Text
 		comando.Parameters.Add("@fechaEntrada", MySqlDbType.Date).Value = fechaEntrada.Value.ToString("yyyy-MM-dd")
 		comando.Parameters.Add("@fechaSalida", MySqlDbType.Date).Value = fechaSalida.Value.ToString("yyyy-MM-dd")
-
-		Dim adapter As New MySqlDataAdapter(comando)
-		Dim tabla As New DataTable()
-
-		adapter.Fill(tabla)
 		Try
-			capacidadDisponible.Text = Integer.Parse(capacidadAloj.Text) - Integer.Parse(tabla(0)(0)) + numeroPersonas.Value
-			If Integer.Parse(capacidadDisponible.Text) < 0 Then
-				capacidadDisponible.Text = 0
-			End If
-		Catch
-			capacidadDisponible.Text = Integer.Parse(capacidadAloj.Text)
+			Dim adapter As New MySqlDataAdapter(comando)
+			Dim tabla As New DataTable()
+
+			adapter.Fill(tabla)
+			Try
+				capacidadDisponible.Text = Integer.Parse(capacidadAloj.Text) - Integer.Parse(tabla(0)(0)) + numeroPersonas.Value
+				If Integer.Parse(capacidadDisponible.Text) < 0 Then
+					capacidadDisponible.Text = 0
+				End If
+			Catch
+				capacidadDisponible.Text = Integer.Parse(capacidadAloj.Text)
+			End Try
+		Catch ex As MySqlException
+			MsgBox("Error al recuperar la ocupacion del alojamiento de la base de datos", MsgBoxStyle.DefaultButton1 + MsgBoxStyle.Critical, "Error")
 		End Try
 	End Sub
 End Class
